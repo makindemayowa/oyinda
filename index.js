41a18971a777840774d043ddff75bc77bfdf6b6a
Mayowa Makinde <mankinde53@yahoo.com>
2018-08-28 01:33:20 +0100

    force response

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let response = prompt('Tell me you love me before you can view this');
+console.log(response)
 
-while (response.toLowerCase() !== "i love you") {
+
+while (!response || response.toLowerCase() !== "i love you") {
   response = prompt('Tell me you love me before you can view this');
 }
-
 if (response.toLowerCase() == "i love you") {
   alert('Love ya too :)')
 }
-
